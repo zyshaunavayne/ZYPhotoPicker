283886cc06741959aa497020e3cb6b5ae60acbdf
zhangyushaunavayne <shaunavayne@vip.qq.com>
2023-04-14 15:53:59 +0800

    第一次提交

--- ZYPhotoPicker/ZYPhotoPicker.h
+++ ZYPhotoPicker/ZYPhotoPicker.h
@@ -7,14 +7,13 @@
 
 #import <Foundation/Foundation.h>
 
+#import <HXPhotoPicker.h>
 #import "ZYPhotoView.h"
 
-//! Project version number for ZYPhotoPicker.
 FOUNDATION_EXPORT double ZYPhotoPickerVersionNumber;
 
-//! Project version string for ZYPhotoPicker.
 FOUNDATION_EXPORT const unsigned char ZYPhotoPickerVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <ZYPhotoPicker/PublicHeader.h>
+
 
 
