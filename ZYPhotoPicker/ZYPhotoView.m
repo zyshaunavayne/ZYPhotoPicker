935356c28b3a5be2646f881fa3fda672d77b04ce
zhangyushaunavayne <shaunavayne@vip.qq.com>
2023-04-17 13:18:13 +0800

    update

--- ZYPhotoPicker/ZYPhotoView.m
+++ ZYPhotoPicker/ZYPhotoView.m
@@ -193,7 +193,7 @@
 - (void)addWithFrame:(CGRect)frame
 {
     self.frame = frame;
-    _photoView.frame = CGRectMake(10, 10, frame.size.width - 20, frame.size.height - 20);
+    _photoView.frame = CGRectMake(10, 0, frame.size.width - 20, frame.size.height - 20);
 }
 
 - (void)setPhotoViewAllHeight:(CGFloat)photoViewAllHeight
